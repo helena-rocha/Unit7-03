f2c3c87f33c3fb9ecd3515df27fad48a171aa66b
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-19 14:59:04 +0000

    i

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function newfunction () {
 } else if (score >= 13) {
   document.getElementById('paragraph').innerHTML = 'You can see a PG-13 movie alone'
 } else if (score >= 5) {
-  document.getElementById('paragraph').innerHTML = 'You can see a G or PG movie alone'
+    document.getElementById('paragraph').innerHTML = 'You can see a G or PG movie alone'
 } else {
-  document.getElementById('paragraph').innerHTML = 'You are too young to watch most movies'
+    document.getElementById('paragraph').innerHTML = 'You are too young to watch most movies'
 }
 }
